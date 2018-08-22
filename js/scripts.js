25764f53454719c98221a04c98a32106409a6bc5
Akjol Jaenbai and Chris Crow <akjol@>
2018-08-22 08:57:09 -0700

    Pass second spec

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,19 @@
+//Business logic
+function NumberCheck(roll){
+  if(roll ===1){alert("Your turn is Over!!")}
+}
+
+
+
 $(document).ready(function(){
   $("#Player1 button#roll").click(function(event){
     event.preventDefault();
 
   var thisRoll = Math.floor((Math.random() * 6) + 1);
   $("#diceRoll").text(thisRoll);
+
+  NumberCheck(thisRoll);
+
 
 });
 
